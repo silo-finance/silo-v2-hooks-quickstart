a503b421c32b5b495bb9ae7245b5d6e6b1a29c36
Neo Racer <0xNeoRacer@protonmail.com>
2025-02-03 12:21:08 +0100

    draft

--- contracts/FullHook.sol
+++ contracts/FullHook.sol
@@ -3,29 +3,38 @@ pragma solidity ^0.8.10;
 
 import {IHookReceiver} from "silo-core-v2/interfaces/IHookReceiver.sol";
 import {ISiloConfig} from "silo-core-v2/interfaces/ISiloConfig.sol";
+import {BaseHookReceiver} from "silo-core-v2/utils/hook-receivers/_common/BaseHookReceiver.sol";
 import {GaugeHookReceiver} from "silo-core-v2/utils/hook-receivers/gauge/GaugeHookReceiver.sol";
 // import {PartialLiquidation} from "silo-v2/core/utils/hook-receivers/liquidation/PartialLiquidation.sol";
 
 contract FullHook is GaugeHookReceiver {
     function initialize(ISiloConfig _siloConfig, bytes calldata _data) external override {
-        GaugeHookReceiver.initialize(_siloConfig, _data);
+//  TODO      super.initialize(_siloConfig, _data);
 
         // put here your code that will be executed on hook initialization
     }
 
-    function hookReceiverConfig(address _silo) external view returns (uint24 hooksBefore, uint24 hooksAfter) {
+    function hookReceiverConfig(address _silo)
+        external
+        view
+        override (BaseHookReceiver, IHookReceiver)
+        returns (uint24 hooksBefore, uint24 hooksAfter)
+    {
         (hooksBefore, hooksAfter) = _hookReceiverConfig(_silo);
 
+        // code
     }
 
-    /// @notice state of Silo before action, can be also without interest, if you need them, call silo.accrueInterest()
-    function beforeAction(address _silo, uint256 _action, bytes calldata _input) external override {
-        GaugeHookReceiver.beforeAction(_silo, _action, _input);
-
+    /// @inheritdoc IHookReceiver
+    function beforeAction(address, uint256, bytes calldata)
+        external
+    {
+        // Silo does not use it.
+        revert RequestNotSupported();
     }
 
-    function afterAction(address _silo, uint256 _action, bytes calldata _inputAndOutput) external override {
-        GaugeHookReceiver.afterAction(_silo, _action, _inputAndOutput);
+    function afterAction(address _silo, uint256 _action, bytes calldata _inputAndOutput) public override {
+        super.afterAction(_silo, _action, _inputAndOutput);
 
     }
 }
