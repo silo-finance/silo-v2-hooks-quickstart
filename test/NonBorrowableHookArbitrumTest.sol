c0f6551f20965ea2b70de81dcb248f342c05aa7d
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2025-02-05 15:26:30 +0100

    Update test/NonBorrowableHookArbitrumTest.sol
    Co-authored-by: IhorSF <102213749+IhorSF@users.noreply.github.com>

--- test/NonBorrowableHookArbitrumTest.sol
+++ test/NonBorrowableHookArbitrumTest.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.28;
 
 import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
-import {Clones} from "openzeppelin5/proxy/Clones.sol";
 
 import {ISiloConfig} from "silo-core-v2/interfaces/ISiloConfig.sol";
 import {ISilo} from "silo-core-v2/interfaces/ISilo.sol";
